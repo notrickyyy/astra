2ef388f1748f6698a63cc6f3c411ca10e5688f61
notrickyyy <68660232+notrickyyy@users.noreply.github.com>
2020-11-14 20:03:29 +0545

    Update linkx.sh

--- linkx.sh
+++ linkx.sh
@@ -1,5 +1,5 @@
 gau $1 | grep "=" | tee $1.txt
 python3 paramspider.py -d $ >> $1.txt
-cat $1.txt | qsreplace "filter" | tee $1_raw.txt
-cat $1_raw.txt | grep -v -e ".svg" -e".woff" -e ".js" -e ".css" -e ".ttf" -e ".eot" > $1_resolved.txt
+cat $1.txt | qsreplace "filter" | sort -u | tee $1_raw.txt
+cat $1_raw.txt | grep -v -e ".svg" -e".woff" -e ".js" -e ".css" -e ".ttf" -e ".eot" | sort -u > $1_resolved.txt
 
